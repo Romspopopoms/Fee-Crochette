2142597773b7048e1286205d2a6721a3231d6fcf
Romain <Romspopopoms@icloud.com>
2025-03-18 11:02:06 +0100

    Implémentation du dashboard admin avec fonctionnalités d'articles

--- pages/api/update-image.js
+++ pages/api/update-image.js
@@ -51,7 +51,6 @@ export default async function handler(req, res) {
         Key: newS3Key,
         Body: fileContent,
         ContentType: newImage.mimetype,
-        ACL: 'public-read', // ✅ Indispensable pour afficher publiquement
       }).promise();
 
       return res.status(200).json({
